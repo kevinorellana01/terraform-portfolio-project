71518575af7dfa2fdd96d1cd6f0bfebd94441c0a
Kevin Orellana <kevin@kevins-air.mynetworksettings.com>
2025-01-06 21:48:50 -0500

    S3 bucket resource

---
+++ state.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+      bucket = "ko-my-terraform-state"
+      key = "global/s3/terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "terraform-lock-file"
+    
+    }
+}
